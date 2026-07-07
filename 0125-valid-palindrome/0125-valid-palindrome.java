5ebb4b86d6980b5df357c1f2976db907efa67c35
Surya <80087951+suryaayrus14@users.noreply.github.com>
2026-07-07 12:43:14 +0530

    Time: 3 ms (60.58%), Space: 44.4 MB (57.34%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s.length() <= 1){
+            return true;
+        }
+       s=s.toLowerCase();
+       int left =0;
+       int right = s.length()-1;
+       String original = s;
+
+       while(left < right){
+        if(!Character.isLetterOrDigit(s.charAt(left))){
+            left++;
+            continue;
+        }
+        if(!Character.isLetterOrDigit(s.charAt(right))){
+            right--;
+            continue;
+        }
+        if (s.charAt(left) != s.charAt(right)){
+            return false;
+        }
+        left++;
+        right--;
+
+
+       }
+       return true;
+}
+}
