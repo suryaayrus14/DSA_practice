53ec3b106694ca8493e7d95ad6483064a8b2c6e5
Surya <80087951+suryaayrus14@users.noreply.github.com>
2026-07-26 15:52:43 +0530

    Time: 30 ms (88.58%), Space: 60.3 MB (12.06%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,58 +1,48 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] arr) {
-        int n = arr.length;
-    Arrays.sort(arr);
-    List<List<Integer>> result = new ArrayList<>();
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        int n = nums.length;
+        
 
+        for(int i =0;i<n;i++){
+            int left = i+1;
+            int right = n-1;
+            int target = 0 - nums[i];
+            if(i > 0 && nums[i] == nums[i-1]){
+                continue;
+            }
 
-for(int i = 0;i<n-2;i++)
-{
-int left = i+1;
-int right = n-1;
+            while(left < right){
+                int sum = nums[left]+nums[right];
+                if(sum == target){
+                    List<Integer> al = new ArrayList<>();
+                    al.add(nums[i]);
+                    al.add(nums[left]);
+                    al.add(nums[right]);
 
-if(i> 0 && arr[i] == arr[i-1])
-{
-continue; 
-}
-while(left<right)
-{
-	int sum = arr[i] + arr[left]+arr[right];
-	
-	if(sum == 0)
-	{
-		List<Integer> al = new ArrayList<>();
-		al.add(arr[i]);
-		al.add(arr[left]);
-		al.add(arr[right]);
-		result.add(al);
-	
-		while(left < right && arr[left] == arr[left+1])
-		{
-			left++;
-		}
-		while(left < right && arr[right] == arr[right-1])
-		{
-			right--;
-		}
-	
-	
-	left++;
-	right--;
-	}
-	else if (sum < 0)
-	{
-		left++;
-	}
-	else{
-		right--;
-	
-	}
+                    result.add(al);
+                     while(left < right && nums[left] == nums[left+1]){
+                    left++;
+                }
+                while(left < right && nums[right] == nums[right-1]){
+                    right--;
+                }
+                left++;
+                right--;
+                }
 
+                if(sum > target){
+                    right--;
+                }
+                else if(sum < target){
+                    left++;
+                }
+                
 
-}
+            }
 
-
-}
-return result;
+        }
+        return result;
     }
 }
